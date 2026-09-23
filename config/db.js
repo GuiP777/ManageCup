e1d5a0853e9aa440d4a8cfd79eeefc1c596c76b2
GuiP777 <gui1danteprado@gmail.com>
2026-09-23 16:30:24 -0300

    não mostra mais sequelize no terminal

--- config/db.js
+++ config/db.js
@@ -1,4 +1,13 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('managecup', 'root', '', {dialect: 'mysql', host: 'localhost',
-query:{raw:true}, define:{timestamps: false, freezeTableName: true,}});
+const sequelize = new Sequelize('managecup', 'root', '', {
+    dialect: 'mysql', host: 'localhost',
+    query: { 
+        raw: true 
+    }, 
+    logging: false,
+    define: { 
+        timestamps: false, 
+        freezeTableName: true, 
+    }
+});
 module.exports = sequelize;
